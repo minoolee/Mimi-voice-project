b9fd615574936572813e0111b849e1fef0a2b718
hamidesafari <hamidesafari606@gmail.com>
2022-09-28 11:45:53 +0200

    fertig1

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,27 +1,32 @@
+import Cart from "./components/Cart";
+import useAlan from "./hooks/useAlan";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import StartPage from "./components/StartPage";
+import Categories from "./components/Categories";
+import MalesStore from "./components/MalesStore";
+import FemalesStore from "./components/FemalesStore";
+import Login from "./components/login";
+import { UserProvider } from "./context/useUser";
+import Account from "./components/account";
 
-import Cart from "./components/Cart"
-import useAlan from "./hooks/useAlan"
-import { BrowserRouter as Router,Route, Routes} from "react-router-dom"
-import StartPage from "./components/StartPage"
-import Categories from "./components/Categories"
-import MalesStore from "./components/MalesStore"
-import FemalesStore from "./components/FemalesStore"
 function App() {
-  useAlan()
+  useAlan();
 
   return (
-    <>
-     <Router>
-    <Routes>
-    <Route path="/" element={<StartPage/>}/>
-    <Route path="/categories" element={<Categories/>}/>
-    <Route path="/males" element={<MalesStore/>}/>
-    <Route path="/females" element={<FemalesStore/>}/>
-    </Routes>
+    <UserProvider>
+      <Router>
+        <Routes>
+          <Route path="/" element={<StartPage />} />
+          <Route path="/categories" element={<Categories />} />
+          <Route path="/males" element={<MalesStore />} />
+          <Route path="/females" element={<FemalesStore />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/account" element={<Account />} />
+        </Routes>
+        <Cart />
       </Router>
-      <Cart />
-    </>
-  )
+    </UserProvider>
+  );
 }
 
-export default App
+export default App;
