7bc5adb053f0a48748bfa948e12601484d0de046
minoolee <yaqoubiamina@gmail.com>
2022-10-07 09:37:53 +0200

    hamideh sorry

---
+++ backend/MalesPerfumes.js
@@ -0,0 +1,297 @@
+const males =[
+  {
+
+  "id": 1,
+  "brand": "VERSACE",
+  "name": "Eros",
+  "price": {
+      "start" : 30,
+      "middel" : 50,
+      "full" : 100,
+    },
+  "target" : "male",
+  "image": "https://media.douglas.de/medias/s6mLvw719124-0-global.jpg?context=bWFzdGVyfGltYWdlc3wxNDkwMTB8aW1hZ2UvanBlZ3xoMzQvaGI1LzE2MTM3NzU5MjYwNzAyL3M2bUx2dzcxOTEyNF8wX2dsb2JhbC5qcGd8ZWIxNzI2OTVkYTIxMDFhZGFhMDBlMTNlNzE1MmUyMjRiOWEwZTY1MjBkZjE2ZTBkYjg2OTc5ZTg5NTY0ZjliOQ&grid=true&imwidth=550"
+},
+{
+  "id": 2,
+  "brand": "VERSACE",
+  "name": "Eros Flame",
+  "price": {
+      "start" : 20,
+      "middel" : 50,
+      "full" : 70,
+    },
+  "target" : "male",
+  "image": "https://media.douglas.de/medias/Gc6Yxd054193-0-global.jpg?context=bWFzdGVyfGltYWdlc3wxMzM5NDh8aW1hZ2UvanBlZ3xoYzcvaDFjLzE2MTMzNDc5ODkwOTc0L0djNll4ZDA1NDE5M18wX2dsb2JhbC5qcGd8M2UzZDkxNTA1YjdlNzcyMWMwMmRkMjhkNDM4OWYxMDliYTdiMTUyMGM1YTg1OWIwOWVmZWVlMmIxNDZmMzVlNw&grid=true&imwidth=550"
+},
+{
+"id": 3,
+  "brand": "VERSACE",
+  "name": "Dylan Blue",
+  "price": {
+      "start" : 35,
+      "middel" : 55,
+      "full" : 95,
+    },
+  "target" : "male",
+  "image": "https://media.douglas.de/medias/1TPdhr932055-0-global.jpg?context=bWFzdGVyfGltYWdlc3w1ODMyN3xpbWFnZS9qcGVnfGhmNi9oMmEvMTYxMzkyNjk1Mzc4MjIvMVRQZGhyOTMyMDU1XzBfZ2xvYmFsLmpwZ3xhOTEyYjA4ODA0MDc1MTFhNTQ1MzNmZjgwMjMyMzYwODY3ZjcxMTQzNmUyYzQxNzUyMGZmYTU1NjI0N2YyYjQ2&grid=true&imwidth=550"
+},
+{
+"id": 4,
+"brand": "JEAN PAUL GAULTIER",
+"name": "Le Beau",
+"price": {
+      "start" : 40,
+      "middel" : 80,
+      "full" : 120,
+    },
+"target" : "male",
+"image": "https://media.douglas.de/medias/q0Xxqz285916-0-global.jpg?context=bWFzdGVyfGltYWdlc3wxNzI5NjN8aW1hZ2UvanBlZ3xoNDAvaDAzLzE3NDY1MTgzMjQwMjIyL3EwWHhxejI4NTkxNl8wX2dsb2JhbC5qcGd8YTY5NGUzMmEzNjZlMWMyNDEwZGMwZWUwNjgxOWJjYzFhZmEzN2EzOGRiMjhkMzJlZGM4ZjlmZjA1MGI5NzU1Yg&grid=true&imwidth=550"
+},
+{
+"id": 5,
+"brand": "JEAN PAUL GAULTIER",
+"name": "Le Male Ultra Intense",
+"price": {
+      "start" : 60,
+      "middel" : 90,
+      "full" : 130,
+    },
+"target" : "male",
+"image": "https://media.douglas.de/medias/ELeBpc614222-0-global.jpg?context=bWFzdGVyfGltYWdlc3w5Njg4MnxpbWFnZS9qcGVnfGhjNS9oNjkvMTc0NjUxODQxNTc3MjYvRUxlQnBjNjE0MjIyXzBfZ2xvYmFsLmpwZ3xiYWI3ZGM0YzU5NmVkNTkzNzI4NGE3MmM5NmQxNWQyMWYzZGNmY2FjYmUxNDA4MDI0YTZiZDNjMzVlOWFlMjUx&grid=true&imwidth=550"
+},
+{
+"id": 6,
+"brand": "JEAN PAUL GAULTIER",
+"name": "Le Male",
+"price": {
+      "start" : 55,
+      "middel" : 85,
+      "full" : 140,
+    },
+"target" : "male",
+"image": "https://media.douglas.de/medias/q7wLeN968696-0-dgl-DE.jpg?context=bWFzdGVyfGltYWdlc3w1MDM4M3xpbWFnZS9qcGVnfGhjYS9oNTUvMTIwOTI2ODcyNTM1MzQvcTd3TGVOOTY4Njk2XzBfZGdsLURFLmpwZ3w0MDQyYzVjMzdiMGFiY2UxODUwN2RlNjAzOWE3MmZlMDRiZmE1ZWJhMzIzYzU3YzY4NDg2NWU1MjBjMmNkNGVm&grid=true&imwidth=550"
+},
+{
+"id": 7,
+"brand": "DIOR",
+"name": "SauvageElixir Duft",
+"price": {
+      "start" : 37,
+      "middel" : 90,
+      "full" : 160,
+    },
+"target" : "male",
+"image":"https://media.douglas.de/medias/Ysit4s242990-0-global.jpg?context=bWFzdGVyfGltYWdlc3w3NjQ3MHxpbWFnZS9qcGVnfGg3ZS9oNjUvMTYxMzQ5OTc5NjY4NzgvWXNpdDRzMjQyOTkwXzBfZ2xvYmFsLmpwZ3xiODQyMTY5NTQwMGM2YzUxN2QzMjM5MTc5NjYzN2VkMTUzOTNkOWU4ODczOTc0ZWI2YjFkZDg5NmM1ODgzZjE1&grid=true&imwidth=550"
+},
+{
+"id": 8,
+"brand": "DIOR",
+"name": "Sauvage",
+"price": {
+      "start" : 85,
+      "middel" : 100,
+      "full" : 180,
+    },
+"target" : "male",
+"image": "https://media.douglas.de/medias/vXie0G995602-0-global.jpg?context=bWFzdGVyfGltYWdlc3w3MjU4OXxpbWFnZS9qcGVnfGgwZC9oMzUvMTMzNDY2MDQyMjA0NDYvdlhpZTBHOTk1NjAyXzBfZ2xvYmFsLmpwZ3wzNmExMDE1N2MxZTcwZWUyM2UxNTNhYzM3NDcyNzE0ZGNmOWY1ZWY0YjhkYjBkOGMzM2Q4MTI4ODk5NTU5NjQx&grid=true&imwidth=550",
+"details": "Duftrichtungblumig: holzig KopfnoteLavendel: Bergamotte HerznoteKardamom: Amber BasisnoteLeder: Patchouli, Vetiver"
+
+},
+{
+"id": 9,
+"brand": "DIOR",
+"name": "Dior HommeIntense",
+"price": {
+      "start" : 80,
+      "middel" : 110,
+      "full" : 200,
+    },
+"target" : "male",
+"image": "https://media.douglas.de/medias/AToodP629708-0-global.jpg?context=bWFzdGVyfGltYWdlc3w5ODY0NnxpbWFnZS9qcGVnfGg2ZS9oZDUvMTM2MjAyNTcwMzAxNzQvQVRvb2RQNjI5NzA4XzBfZ2xvYmFsLmpwZ3wyMzgyYzRmMDAyZTJiNDhlZTlhNDE2MmVjOTE1ZGQ2ZGE1ZjcxZjY1NWIzODVlMTQyMjQxMDg4ZjAzZDllNmY2&grid=true&imwidth=550"
+},
+{
+"id": 10,
+"brand": "DIOR",
+"name": "Dior Homme",
+"price": {
+      "start" : 70,
+      "middel" : 100,
+      "full" : 180,
+    },
+"target" : "male",
+"image": "https://media.douglas.de/medias/Wxpd2u839738-0-global.jpg?context=bWFzdGVyfGltYWdlc3w2NTYzOXxpbWFnZS9qcGVnfGhjNi9oYzEvMTMzNDU2Mjg4ODA5MjYvV3hwZDJ1ODM5NzM4XzBfZ2xvYmFsLmpwZ3w5MjJlNjg5NzYxMzRiZjgyNmNiMWFlNmI0YmRlMmZhYmMxZWM5N2FjNGNkYzg0NzU4NWM5MzJhM2ZlMjY0YWNm&grid=true&imwidth=550"
+
+},
+{
+"id": 11,
+"brand": "BOSS",
+"name": "EdT",
+"price": {
+      "start" : 85,
+      "middel" : 120,
+      "full" : 220,
+    },
+"target" : "male",
+"image": "https://media.douglas.de/medias/Bsnx07880763-0-global.jpg?context=bWFzdGVyfGltYWdlc3w2MzM2NHxpbWFnZS9qcGVnfGg5Ny9oYjMvMTYxMzg1NTU3ODUyNDYvQnNueDA3ODgwNzYzXzBfZ2xvYmFsLmpwZ3w2ZWFkNjAzOTJhZjg3NTEwMjdjNjIwNTQ0MDYwMDVlYTBjOWMyNzQ1MDFmNmZiMDgxM2I4OWYxN2E4MjI5M2Nm&grid=true&imwidth=550"
+},
+{
+"id": 12,
+"brand": "BOSS",
+"name": "Boss The ScentAbsolute For Him",
+"price": {
+      "start" : 90,
+      "middel" : 130,
+      "full" : 260,
+    },
+"target" : "male",
+"image": "https://media.douglas.de/medias/SS6TCc266251-0-global.jpg?context=bWFzdGVyfGltYWdlc3w4NDUxMXxpbWFnZS9qcGVnfGhlMC9oODAvMTYxMzUwMjgxMTM0MzgvU1M2VENjMjY2MjUxXzBfZ2xvYmFsLmpwZ3wzN2E5NzI2MzU1MGQ1NjUxZGYxNzQzYjJmNzI0NjZhYjA2ODZiMDM2NzhjNzAwOTJlMTJjYzQ4MjkzYmI0YmRh&grid=true&imwidth=550"
+},
+{
+"id": 13,
+"brand": "BOSS",
+"name": "Boss BottledEau de Parfum Spray",
+"price": {
+      "start" : 450,
+      "middel" : 90,
+      "full" : 140,
+    },
+"target" : "male",
+"image": "https://media.douglas.de/medias/GUAa91707000-0-dgl-DE.jpg?context=bWFzdGVyfGltYWdlc3wxMjk0ODB8aW1hZ2UvanBlZ3xoMDEvaGQ4LzE3NTg5Mzg5MzYxMTgyL0dVQWE5MTcwNzAwMF8wX2RnbC1ERS5qcGd8ZGM0Y2Y4MDU2N2JhZDdkOWZiMTg5NzRiOTA5NmU3Y2UzN2U4ZTAzNTc1MDA1YjhhMjczYzAwOTRkNmEzMDBiYw&grid=true&imwidth=550"
+},
+{
+"id": 14,
+"brand": "BOSS",
+"name": "Boss Bottled",
+"price": {
+      "start" : 85,
+      "middel" : 150,
+      "full" : 250,
+    },
+"target" : "male",
+"image": "https://media.douglas.de/medias/P9FPZh397211-0-global.jpg?context=bWFzdGVyfGltYWdlc3w0NzE4OXxpbWFnZS9qcGVnfGg1Yy9oMWEvMTYxMzU3MTg4OTU2NDYvUDlGUFpoMzk3MjExXzBfZ2xvYmFsLmpwZ3w0YjI4YzRhNzBkOWEyMjAxYzE3MWJkNTlhNTRmYmQ4M2NkOTRiN2QyZTU1ZjllOWNmMzQxNWFhZWJkNjFjYzli&grid=true&imwidth=550"
+},
+{
+"id": 15,
+"brand": "ARMANI",
+"name": "Code Homme",
+"price": {
+      "start" : 70,
+      "middel" : 90,
+      "full" : 150,
+    },
+"target" : "male",
+"image": "https://media.douglas.de/medias/DEVYR9257635-0-global.jpg?context=bWFzdGVyfGltYWdlc3w0NTY4NXxpbWFnZS9qcGVnfGhhNi9oODkvMTMzNDI2NTAwNDAzNTAvREVWWVI5MjU3NjM1XzBfZ2xvYmFsLmpwZ3xjOTRjMDA5ODE0ZGY0YmZjN2I1NTAyNTFlNWE2NGEwYzg3ZDVlMTE4OTI4Njc0Mzk2NThkMTIzMWRlZmFlMDFl&grid=true&imwidth=550"
+},
+{
+"id": 16,
+"brand": "ARMANI",
+"name": "Acqua di Giò Homme",
+"price": {
+      "start" : 55,
+      "middel" : 120,
+      "full" : 190,
+    },
+"target" : "male",
+"image": "https://media.douglas.de/medias/PAkesL836635-0-global.jpg?context=bWFzdGVyfGltYWdlc3w2MjgwNnxpbWFnZS9qcGVnfGgwOS9oMTAvMTMzNDU2MTI3NTkwNzAvUEFrZXNMODM2NjM1XzBfZ2xvYmFsLmpwZ3w2MDI2OGY3YTE2OWEyNjliYTI5ZTdhNmQwYTNhMTU5OGU0YmYyZTY0NGE3ZTA4ZTY4MGYzMzk1M2EzY2UyNzc1&grid=true&imwidth=550"
+},
+{
+"id": 17,
+"brand": "ARMANI",
+"name": "Acqua di Giò HommeProfondo",
+"price": {
+      "start" : 60,
+      "middel" : 75,
+      "full" : 145,
+    },
+"target" : "male",
+"image": "https://media.douglas.de/medias/OvObV5522003-0-dgl-DE.jpg?context=bWFzdGVyfGltYWdlc3w1MTIwMXxpbWFnZS9qcGVnfGhhMC9oMmEvMTAwMjkzNTk5ODg3NjYvT3ZPYlY1NTIyMDAzXzBfZGdsLURFLmpwZ3w0ZmYxOWFhMjlmMWJhMDc5ODUzYzY0Zjg3ZTU0YjJmOGNmMWQ5OTE5MTI1ZThjNjk5NjlhMWNkNTkyY2JmOTE3&grid=true&imwidth=550"
+},
+{
+"id": 18,
+"brand": "PRADA",
+"name": "Luna Rossa Ocean",
+"price": {
+      "start" : 65,
+      "middel" : 95,
+      "full" : 175,
+    },
+"target" : "male",
+"image": "https://media.douglas.de/medias/mUfzxs218438-0-dgl-DE.jpg?context=bWFzdGVyfGltYWdlc3w1MTk5NXxpbWFnZS9qcGVnfGhhMC9oNGQvMTE0MDQ2MDQ1Mzg5MTAvbVVmenhzMjE4NDM4XzBfZGdsLURFLmpwZ3w2MjY0OTA2ZjliNjA3YzU3NWZjYTE1NzY1OWM5MmMwYWI5OWYyYTc4NzNjZmIyMGEzYTRjMDhkNWQ3ZGFlOTY3&grid=true&imwidth=550"
+},
+{
+"id": 19,
+"brand": "PRADA",
+"name": "Luna Rossa Carbon",
+"price": {
+      "start" : 55,
+      "middel" : 85,
+      "full" : 140,
+    },
+"target" : "male",
+"image": "https://media.douglas.de/medias/iAJwWL959956-0-global.jpg?context=bWFzdGVyfGltYWdlc3w3MDk1OXxpbWFnZS9qcGVnfGhhMy9oNjgvMTMzNDYyNDQyOTY3MzQvaUFKd1dMOTU5OTU2XzBfZ2xvYmFsLmpwZ3xjYjY0ZmU1NDVlMmIwMTA0NGM1OGFjNjU2MzliNzMxNjFhODlmYjA4MzQxZjUyZDNkMDY2NDM3MTc0Mzk4MTBk&grid=true&imwidth=550"
+},
+{
+"id": 20,
+"brand": "PRADA",
+"name": "L Homme",
+"price": {
+      "start" : 70,
+      "middel" : 90,
+      "full" : 160,
+    },
+"target" : "male",
+"image": "https://media.douglas.de/medias/MS7BcR936318-0-global.jpg?context=bWFzdGVyfGltYWdlc3w5NjEzMHxpbWFnZS9qcGVnfGg0Mi9oMGIvMTYxMzkzNDAzMTY3MDIvTVM3QmNSOTM2MzE4XzBfZ2xvYmFsLmpwZ3wxMzdiOThhYjljOGYzOGQxMmI4ZWFhYjZjYzAyOWU4OGQ1YTEzYTYyMTU5Y2RmZTI2YjQzNmI1NTg4ZjJkYzky&grid=true&imwidth=550"
+},
+{
+"id": 21,
+"brand": "PRADA",
+"name": "L'Homme L'Eau",
+"price": {
+      "start" : 75,
+      "middel" : 95,
+      "full" : 160,
+    },
+"target" : "male",
+"image": "https://media.douglas.de/medias/VQ98RJ994669-0-global.jpg?context=bWFzdGVyfGltYWdlc3wxMDE1MzR8aW1hZ2UvanBlZ3xoYmYvaDUxLzEzMzQ2NDY0MDM4OTQyL1ZROThSSjk5NDY2OV8wX2dsb2JhbC5qcGd8NjMxNzNmYjNiZDc0MGI1MDI1YzlkMWZhMjY5OWQ1MjA5YWZmZDZjMjc0MWE5NGM4MGZjZjhhM2ZhZjM3M2Q5YQ&grid=true"
+},
+{
+"id": 22,
+"brand": "GUERLAIN",
+"name": "L’Homme Idéal",
+"price": {
+      "start" : 80,
+      "middel" : 100,
+      "full" : 170,
+    },
+"target" : "male",
+"image":"https://media.douglas.de/medias/1ubtRS924499-0-dgl-DE.jpg?context=bWFzdGVyfGltYWdlc3wzMjIwMDN8aW1hZ2UvanBlZ3xoOTAvaGI0LzE2ODY0MDkxMTQ0MjIyLzF1YnRSUzkyNDQ5OV8wX2RnbC1ERS5qcGd8YzY1NDk1NDkyYzQ3OGI5ZDhiYTk3YmFjMDY1OGE3MDA0YWZhOWZmZmJiMDE3ZmFiMjJjNmE2NmI0YTUyOTc2MQ&grid=true&imwidth=550"
+},
+{
+  "id": 23,
+"brand": "GUERLAIN",
+"name": "L’Homme IdéalExtreme",
+"price": {
+      "start" : 95,
+      "middel" : 150,
+      "full" : 240,
+    },
+"target" : "male",
+"image":"https://media.douglas.de/medias/RgSrdZ628589-0-dgl-DE.jpg?context=bWFzdGVyfGltYWdlc3wzMjMwMDR8aW1hZ2UvanBlZ3xoYTYvaGViLzE2ODY0MDkwNDIzMzI2L1JnU3JkWjYyODU4OV8wX2RnbC1ERS5qcGd8NDY2NjZjNTdiYzExZDAwODMxM2Q2ZDQ3NWMwMzI5MjM1NmRhZWZmMzM5MjcxZjEyZGNmZDBjNDlkZjRlOGQ1NA&grid=true&imwidth=550"
+},
+{
+  "id": 24,
+"brand": "GUERLAIN",
+"name": "L’Instant de Guerlain Pour Homme",
+"price": {
+      "start" : 70,
+      "middel" : 150,
+      "full" : 230,
+    },
+"target" : "male",
+"image":"https://media.douglas.de/medias/c9etNH1001537-0-global.jpg?context=bWFzdGVyfGltYWdlc3wzMjQ1ODF8aW1hZ2UvanBlZ3xoMTQvaDAwLzE2MTY1OTY0MDU0NTU4L2M5ZXROSDEwMDE1MzdfMF9nbG9iYWwuanBnfDhlNjY5ODUyZmJhNTNhNDM2ODhlMzcyMWI0NTgwMTFlYWZkYzNhODMxMzJhZGI5YjRmOWQxN2NkZGY5MDE0NjI&grid=true&imwidth=550"
+}
+]
+
+
+module.exports = males
