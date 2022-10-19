2981bd4a8170d6289826fd5d1e8c00472d436666
minoolee <yaqoubiamina@gmail.com>
2022-10-19 11:33:42 +0200

    thank

---
+++ frontend/src/components/Thank.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Thank() {
+  return (
+    <div>
+        <h1>Thank An</h1>
+        <h3>Our Teacher</h3>
+        
+    </div>
+  )
+}
+
+export default Thank
